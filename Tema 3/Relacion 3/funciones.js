f7792c758ab5d1c8f40e8be2d02a57703727810b
imartor99 <imartor769@g.educaand.es>
2025-10-18 16:57:53 +0200

    realizo ej1

--- Tema 3/Relacion 3/funciones.js
+++ Tema 3/Relacion 3/funciones.js
@@ -1,1 +1,14 @@
-// EJERCICIO 1
+function mostrarClase(arrayClase) {
+    // Generamos el título de la sección.
+    document.write("<h2>Detalle de Alumnos:</h2>");
+
+    for (let i = 0; i < arrayClase.length; i++) {
+        const elemento = arrayClase[i];
+
+        document.write(`
+            <p>
+                clase[${i}] = “${elemento}”
+            </p>
+        `);
+    }
+}
