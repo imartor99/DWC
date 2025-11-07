ae3de914990d918e7153cf3fad0050b480b47b9a
imartor99 <imartor769@g.educaand.es>
2025-11-07 10:16:02 +0100

    estructuro

---
+++ Tema 4/Relacion1/ej4/main4.js
@@ -0,0 +1,12 @@
+/* 4. Eventos onMouseMove y onLoad. Debemos ser capaces de dibujar con nuestro ratón
+en dos colores diferentes. Para ello primero simularemos que disponemos de un
+canvas gráfico: realmente se tratará de una tabla con celdas de pequeño tamaño
+(100x100 puede valer). Tu programa creará ese canvas una vez cargada la página
+(onLoad). Lo siguiente será detectar el movimiento del ratón sobre las celdas para
+pintarlas de un color, el cual será rojo si se mantiene pulsada simultaneamente la
+tecla Ctrl y azul si se pulsa Shift. En otro caso no deberá pintarse nada.*/
+
+
+(()=>{
+    
+})
